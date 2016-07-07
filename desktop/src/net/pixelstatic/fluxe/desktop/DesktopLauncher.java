6c807b9ac5a5c0ec291f9f1f360041574d38718d
BuildTools <arnukren@gmail.com>
2016-07-07 13:50:35 -0400

    Added gradle projects.

---
+++ desktop/src/net/pixelstatic/fluxe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.pixelstatic.fluxe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.pixelstatic.fluxe.Fluxe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Fluxe(), config);
+	}
+}

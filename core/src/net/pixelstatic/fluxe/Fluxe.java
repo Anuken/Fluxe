40e4daadbd311b7beab333087f75b4de8aff7b45
BuildTools <arnukren@gmail.com>
2016-07-07 16:35:43 -0400

    Added ModelFactory class.

--- core/src/net/pixelstatic/fluxe/Fluxe.java
+++ core/src/net/pixelstatic/fluxe/Fluxe.java
@@ -1,27 +1,13 @@
 package net.pixelstatic.fluxe;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import net.pixelstatic.fluxe.modules.Editor;
+import net.pixelstatic.utils.modules.ModuleController;
 
-public class Fluxe extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Fluxe extends ModuleController<Fluxe>{
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void init(){
+		addModule(Editor.class);
 	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+	
 }
--- desktop/src/net/pixelstatic/fluxe/desktop/DesktopLauncher.java
+++ desktop/src/net/pixelstatic/fluxe/desktop/DesktopLauncher.java
@@ -1,12 +1,20 @@
 package net.pixelstatic.fluxe.desktop;
 
+import java.awt.Toolkit;
+
+import net.pixelstatic.fluxe.Fluxe;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import net.pixelstatic.fluxe.Fluxe;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		Toolkit tool = Toolkit.getDefaultToolkit();
+		config.width = tool.getScreenSize().width;
+		config.height = tool.getScreenSize().height;
+		
 		new LwjglApplication(new Fluxe(), config);
 	}
 }
